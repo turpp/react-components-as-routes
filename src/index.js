cc69e2e00a7adaf5d254aa6b6dd898428ab93863
turpp <white.tripp@icloud.com>
2021-02-15 16:28:10 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,112 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+// /* Add basic styling for NavLinks */
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white',
+// }
+
+// /* add the navbar component */
+// const Navbar = () =>
+//   <div>
+//     <NavLink
+//       to="/"
+//       /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+//       exact
+//       /* add styling to Navlink */
+//       style={link}
+//       /* add prop for activeStyle */
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Home</NavLink>
+//     <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >About</NavLink>
+//     <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Login</NavLink>
+//   </div>;
+
+// ReactDOM.render((
+//   <Router>
+//     <div>
+//       <Navbar/>
+//     <Route exact path="/" component={Home} />
+//     <Route exact path="/about" component={About} />
+//     <Route exact path="/login" component={Login}/>
+//     </div>
+//   </Router>),
+//   document.getElementById('root')
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './home'
+import About from './about'
+import Login from './login'
+import Navbar from './navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Home = () => {
-  return (
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
